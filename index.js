d7dc1062765f285c2428c66cd6038646ae243b45
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-09-07 16:35:20 -0500

    Updated index.html and index.js files
    In the HTML file, I added the Unordered List Element. In the JS file, I added the ability to display new items in the myLeads array.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // initial variables 
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 let myLeads = []
 
 // input button click function
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    renderLead()
     inputEl.value = ''
 })
+
+// Display new item in myLeads array
+function renderLead() {
+    let listItem = `<li><a href=${inputEl.value} target="_blank">${inputEl.value}</a></li>`
+    ulEl.innerHTML += listItem
+}
+
+ 
