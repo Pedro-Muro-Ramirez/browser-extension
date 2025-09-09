ca33c0e5e45986e0a579e18e65cb937c8f4172c9
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-09-08 19:42:41 -0500

    update CSS, HTML and JS
    Added the Delete button to clear the localestorage and DOM.

--- index.js
+++ index.js
@@ -2,14 +2,22 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 let myLeads = []
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 // Display Leads on initial load if it has items in the myLeads array
 if (leadsFromLocalStorage) {
         myLeads = leadsFromLocalStorage
         renderLead()
     }
+
+// Delete button code to clear the storage and DOM
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLead()
+})
 
 // input button click function
 inputBtn.addEventListener("click", function() {
