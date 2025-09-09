915bc4237160b334469d937c1155b2ac400be34f
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-09-08 19:28:22 -0500

    Updated index.js
    Add the ability to display leads from LocaleStorage so they don't get removed on page refresh.

--- index.js
+++ index.js
@@ -3,18 +3,28 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 let myLeads = []
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+// Display Leads on initial load if it has items in the myLeads array
+if (leadsFromLocalStorage) {
+        myLeads = leadsFromLocalStorage
+        renderLead()
+    }
 
 // input button click function
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    renderLead()
-    inputEl.value = ''
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLead()  
+    inputEl.value = ''  
 })
 
 // Display new item in myLeads array
 function renderLead() {
-    let listItem = `<li><a href=${inputEl.value} target="_blank">${inputEl.value}</a></li>`
-    ulEl.innerHTML += listItem
+    const listItems = myLeads.map(lead => {
+        return `<li><a href="${lead}" target="_blank">${lead}</a></li>`
+    })
+    ulEl.innerHTML = listItems.join("")
 }
 
  
