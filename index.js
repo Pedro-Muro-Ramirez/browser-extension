6182cf67e27c59eab8590290b510a3c11fd1f557
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-09-09 21:22:19 -0500

    Updated index, js and manifest files
    Added the tab-btn to the HMTL file. Added the functionality to display the current tab within the myLeads array. Added permissions for Chrome to allow the use of tabs in the manifest file.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // initial variables 
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 let myLeads = []
@@ -9,27 +10,36 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 // Display Leads on initial load if it has items in the myLeads array
 if (leadsFromLocalStorage) {
         myLeads = leadsFromLocalStorage
-        renderLead()
+        render(myLeads)
     }
 
 // Delete button code to clear the storage and DOM
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLead()
+    render(myLeads)
+})
+
+// tabBtn function to display the current tab URL
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 // input button click function
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLead()  
+    render(myLeads)  
     inputEl.value = ''  
 })
 
 // Display new item in myLeads array
-function renderLead() {
-    const listItems = myLeads.map(lead => {
+function render(leads) {
+    const listItems = leads.map(lead => {
         return `<li><a href="${lead}" target="_blank">${lead}</a></li>`
     })
     ulEl.innerHTML = listItems.join("")
